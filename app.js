4b42d613d35d5cf9667b8084566386bcc3ce8d35
jlukic <jacklukic@gmail.com>
2014-11-08 14:04:18 -0500

    Initial work on sample API

--- app.js
+++ app.js
@@ -1,8 +1,72 @@
-var express = require('express')
-, app = express()
+var
+  express   = require('express'),
+  mysql     = require('mysql'),
+  Sequelize = require('sequelize'),
 
-app.get('/', function(req, res){
-	res.send('Hello World')
+  config    = require('./config/server'),
+  app       = express(),
+
+  sequelize = new Sequelize(config.sql.database, config.sql.user, config.sql.password, {
+    dialect : 'mysql',
+    port    :    3306,
+  }),
+  apiResponse = { success: true }
+;
+
+
+app.get('/', function(request, response) {
+  response.send('Semantic API');
+});
+
+app.get('/search', function(request, response) {
+  response.send('Please specify a search term');
+});
+
+app.get('/search/:query', function(request, response, next) {
+  var
+    Animals   = sequelize.define('Animals', config.schema.animals, config.table.animals),
+    Passwords = sequelize.define('Passwords', config.schema.passwords, config.table.passwords),
+    Pets      = sequelize.define('Pets', config.schema.pets, config.table.pets),
+    query     = request.param('query') || false,
+    category,
+    results
+  ;
+
+  if(!query) {
+    response.send('Please specify a search term');
+  }
+  sequelize
+    .authenticate()
+    .complete(function() {
+      Animals
+        .findAndCountAll({
+          where: [
+            "name LIKE '"+query+"%'"
+          ],
+          limit: 5
+        })
+        .complete(function(error, results) {
+          if(!!error) {
+            response.send('An error occurred while searching', error);
+          }
+          if(results && results.count > 0) {
+            category.name = 'Animals';
+            category.results = {};
+            for(var index in results) {
+              var animal = results[index];
+              // push animal to results
+              category.results.push({
+                title       : animal.name,
+                description : animal.status
+              });
+            }
+            apiResponse.results.push(category);
+          }
+        })
+      ;
+      response.json(apiResponse);
+    })
+  ;
 });
 
 app.listen(3000);
--- readme.md
+++
@@ -1,1 +0,0 @@
-Express app with incredible functionality to display 'Hello World' on command!
